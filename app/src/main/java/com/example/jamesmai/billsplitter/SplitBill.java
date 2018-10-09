1e4909220946666bd54b8517b5101538ab161f88
BeAmazedVariable <43376325+BeAmazedVariable@users.noreply.github.com>
2018-10-08 20:25:47 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jamesmai/billsplitter/SplitBill.java
@@ -0,0 +1,15 @@
+package com.example.jamesmai.billsplitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplitBill extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_split_bill);
+    }
+}
